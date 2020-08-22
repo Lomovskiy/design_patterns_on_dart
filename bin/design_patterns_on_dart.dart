4fbd200c2faaffae24753b78219f5c03de9386b6
Alexander Lomovskiy <a@lomovskiy.com>
2020-08-22 09:32:04 +0300

    Initial commit

---
+++ bin/design_patterns_on_dart.dart
@@ -0,0 +1,5 @@
+import 'package:design_patterns_on_dart/design_patterns_on_dart.dart' as design_patterns_on_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${design_patterns_on_dart.calculate()}!');
+}
